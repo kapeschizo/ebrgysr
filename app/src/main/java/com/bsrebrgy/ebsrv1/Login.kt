c30cf733c25f952349cf983823e1fdd7320a7678
kapeschizo <60978681+kapeschizo@users.noreply.github.com>
2021-05-25 07:39:29 +0800

    add login and register function

--- app/src/main/java/com/bsrebrgy/ebsrv1/Login.kt
+++ app/src/main/java/com/bsrebrgy/ebsrv1/Login.kt
@@ -4,9 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.util.HashMap
 
 class Login : AppCompatActivity() {
+    var userLoginEtxt: EditText? = null
+    var passLoginETxt: EditText? = null
+    var loginBtn: Button? = null
+    var url: kotlin.String = "http://192.168.1.9/Ebrgy/API/loginAPI.php"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -16,5 +28,54 @@ class Login : AppCompatActivity() {
             val signUpIntent = Intent(this, Register::class.java)
             startActivity(signUpIntent)
         }
+
+        userLoginEtxt = findViewById<EditText>(R.id.userLoginEtxt)
+        passLoginETxt = findViewById<EditText>(R.id.passLoginEtxt)
+        loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn!!.setOnClickListener {
+            login(
+                userLoginEtxt!!.text.toString(),
+                passLoginETxt!!.text.toString()
+            )
+        }
+    }
+    private fun login(user: String, pass: String) {
+
+        val request: StringRequest =
+            object : StringRequest(Method.POST, url, Response.Listener { response ->
+                userLoginEtxt!!.setText(" ")
+                passLoginETxt!!.setText(" ")
+                Toast.makeText(applicationContext, response, Toast.LENGTH_LONG).show()
+
+                if (response == "Register Successful") {
+                    val loginIntent = Intent(this, Login::class.java)
+                    startActivity(loginIntent)
+                }
+
+            }, Response.ErrorListener { error ->
+                userLoginEtxt!!.setText(" ")
+                passLoginETxt!!.setText(" ")
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
+            }
+            ) {
+                @Throws(AuthFailureError::class)
+                override fun getParams(): kotlin.collections.MutableMap<String, String> {
+                    val map: kotlin.collections.MutableMap<String, String> =
+                        HashMap<String, String>()
+                    map.put("user", user)
+                    map.put("pass", pass)
+                    return map
+                }
+            }
+
+        request.retryPolicy = DefaultRetryPolicy(
+            0,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+
+        val queue = Volley.newRequestQueue(applicationContext)
+        queue.add(request)
+
     }
 }
--- app/src/main/java/com/bsrebrgy/ebsrv1/Register.kt
+++ app/src/main/java/com/bsrebrgy/ebsrv1/Register.kt
@@ -1,11 +1,76 @@
 package com.bsrebrgy.ebsrv1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.util.*
+
 
 class Register : AppCompatActivity() {
+    var userRegEtxt: EditText? = null
+    var emailRegEtxt: EditText? = null
+    var signUpBtn: Button? = null
+    var url: String = "http://192.168.1.9/Ebrgy/API/sendEmail.php"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        userRegEtxt = findViewById<EditText>(R.id.userSignEtxt)
+        emailRegEtxt = findViewById<EditText>(R.id.emailSignEtxt)
+        signUpBtn = findViewById<Button>(R.id.signUpBtn)
+
+        signUpBtn!!.setOnClickListener {
+            register(
+                userRegEtxt!!.text.toString(),
+                emailRegEtxt!!.text.toString()
+            )
+        }
+    }
+
+    private fun register(user: String, email: String) {
+
+        val request: StringRequest =
+            object : StringRequest(Method.POST, url, Response.Listener { response ->
+                userRegEtxt!!.setText(" ")
+                emailRegEtxt!!.setText(" ")
+                Toast.makeText(applicationContext, response, Toast.LENGTH_LONG).show()
+
+                if (response == "Register Successful") {
+                    val loginIntent = Intent(this, Login::class.java)
+                    startActivity(loginIntent)
+                }
+
+            }, Response.ErrorListener { error ->
+                userRegEtxt!!.setText(" ")
+                emailRegEtxt!!.setText(" ")
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
+            }
+            ) {
+                @Throws(AuthFailureError::class)
+                override fun getParams(): kotlin.collections.MutableMap<String, String> {
+                    val map: kotlin.collections.MutableMap<String, String> =
+                        HashMap<String, String>()
+                    map.put("user", user)
+                    map.put("email", email)
+                    return map
+                }
+            }
+
+        request.retryPolicy = DefaultRetryPolicy(
+            0,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+
+        val queue = Volley.newRequestQueue(applicationContext)
+        queue.add(request)
+
     }
 }
